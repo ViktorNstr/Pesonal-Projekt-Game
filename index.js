27fc9cc923a6210071bb9b038db6c2220caa9bec
Viktor N <viktor.nestorov@web.de>
2023-05-24 17:34:02 +0200

    Some errors

--- index.js
+++ index.js
@@ -15,27 +15,30 @@ class Game {
     this.backgroundSound = document.getElementById("backgroundSound");
     this.backgroundSound.play();
     this.villain.width = this.villain.clientWidth;
-
-    }
-	function startGame() {
-      const game = new Game();
-      game.moveVillain();
-    }
-
+    this.counter = 0;
     this.hero.addEventListener("mousedown", () => {
       const clickSound = document.getElementById("clickSound");
       clickSound.play();
     });
+
+  }
+
+  function startGame() {
+    const game = new Game();
+    game.moveVillain();
+  }
+  
+
     const startButton = document.getElementById("startButton");
     startButton.addEventListener("click", function() {
-    startGame(); // Call the startGame() function directly
+    startGame(); 
     });
 
     function resetGame() {
-    // Update the position of the hero element
-    var heroUp = getRandomHeroPosition();
+    
+    let heroUp = getRandomHeroPosition();
     hero.style.top = heroTop + "px";
-	var villainLeft = getRandomVillainPosition();
+	let villainLeft = getRandomVillainPosition();
     villain.style.left = villainLeft + "px";
     }
      this.jumpRightButton.addEventListener("mousedown", () => {
@@ -67,43 +70,43 @@ for (let i = 0; i < 5; i++) {
   gameBoard.appendChild(newvillain);
 }
   
-  // Call the addClones function when the page loads to add initial clones
+  
   window.onload = addClones;
 
-  // Call the addClones function when the player successfully avoids a villain
+  
   if (villainLeft < -20) {
   addClones();
   }
   
   function jump(){
-    if(hero.classList == "animate"){return}
-    hero.classList.add("animate");
+    if(hero.classList == "jumping"){return}
+    hero.classList.add("jumping");
 
-    // Increase jump height for level 2
+    
     if (counter >= 1000 && counter < 2000) {
         setTimeout(function(){
             hero.style.top = "120px";
             setTimeout(function(){
                 hero.style.top = "150px";
-                hero.classList.remove("animate");
+                hero.classList.remove("jumping");
             }, 300);
         }, 0);
     }
 
-    // Increase jump height for level 3
+    
     else if (counter >= 2000) {
         setTimeout(function(){
             hero.style.top = "100px";
             setTimeout(function(){
                 hero.style.top = "150px";
-                hero.classList.remove("animate");
+                hero.classList.remove("jumping");
             }, 300);
         }, 0);
     }
 
     else {
         setTimeout(function(){
-            hero.classList.remove("animate");
+            hero.classList.remove("jumping");
         },300);
     }
 }
@@ -120,7 +123,10 @@ Similarly, you can modify the villan animation to increase its speed when the pl
     if (this.checkCollision()) {
       this.gameOver();
     }
-  
+    if (this.villainLeft < -20) {
+      this.addClones();
+    }
+
     window.requestAnimationFrame(() => this.moveVillain());
   }
   
@@ -143,7 +149,7 @@ Similarly, you can modify the villan animation to increase its speed when the pl
     setTimeout(() => {
       this.hero.classList.remove("jumping");
       this.hero.classList.remove("jump-right");
-      this.checkDead();
+      this.checkResult();
     }, 300);
   }
 
@@ -154,7 +160,7 @@ Similarly, you can modify the villan animation to increase its speed when the pl
     setTimeout(() => {
       this.hero.classList.remove("jumping");
       this.hero.classList.remove("jump-left");
-      this.checkDead();
+      this.checkResult();
     }, 300);
   }
 
@@ -168,29 +174,29 @@ Similarly, you can modify the villan animation to increase its speed when the pl
     this.youLost.style.display = "flex";
   }
 
-  var checkResult = setInterval(function() {
+  function checkResult() {
     let heroTop = parseInt(window.getComputedStyle(hero).getPropertyValue("top"));
     let villainLeft = parseInt(window.getComputedStyle(villain).getPropertyValue("left"));
+
     if (villainLeft < 20 && villainLeft > -20 && heroTop >= 130) {
-        villain.style.animation = "none";
-        clearInterval(checkDead);
-        alert("Game Over. score: " + Math.floor(counter/100));
-        counter = 0;
-        villain.style.animation = "villain 1s infinite linear";
+      villain.style.animation = "none";
+      clearInterval(checkResult);
+      alert("Game Over. score: " + Math.floor(this.counter/100));
+      this.counter = 0;
+      villain.style.animation = "villain 1s infinite linear";
 
-        // Show the youLost element
-        document.getElementById("youLost").style.display = "villain";
+      
+      document.getElementById("youLost").style.display = "flex";
     } else {
-        counter++;
-        document.getElementById("p").innerHTML = Math.floor(counter/100);
+      this.counter++;
+      document.getElementById("p").innerHTML = Math.floor(this.counter/100);
 
-        // Check if the player has won
-        if (counter >= 3000) {
-            clearInterval(checkDead);
-            alert("You won!");
-            counter = 0;
-            document.getElementById("youWon").style.display = "villain";
-        }
+      
+      if (this.counter >= 3000) {
+        clearInterval(checkResult);
+        alert("You won!");
+        this.counter = 0;
+        document.getElementById("youWon").style.display = "flex";
+      }
     }
-}, 10);
-  
+  }
